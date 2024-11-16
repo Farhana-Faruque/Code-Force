515dd553d24eaf732cbca383334840b325351282
Kazi Farhana Faruque <168679096+Farhana-Faruque@users.noreply.github.com>
2024-11-16 21:33:05 +0600

    Add files via upload

---
+++ WrongSubtraction.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    int a,b;
+    scanf("%d %d", &a, &b);
+    for(int i = 0; i < b; i++){
+        if(a%10 == 0){
+            a = a/10;
+        }
+        else{
+            a = a-1;
+        }
+    }
+    printf("%d",a);
+    return 0;
+}
