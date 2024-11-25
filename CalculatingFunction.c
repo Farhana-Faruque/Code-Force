54c560c0ddf609f061feaa7fdddf2c362a682794
Kazi Farhana Faruque <168679096+Farhana-Faruque@users.noreply.github.com>
2024-11-26 03:33:38 +0600

    Add files via upload

---
+++ CalculatingFunction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+    long long int n;
+    scanf("%lld",&n);
+    if(n%2 == 0){
+        printf("%lld",(n/2));
+    }
+    else{
+        printf("%lld",(-((n+1)/2)));
+    }
+    
+    return 0;
+}
